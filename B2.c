973a7c9785d223397aea70d35112dc77b0922cc1
luismeruje <luismeruje@gmail.com>
2016-03-20 21:46:17 +0000

    primeiro submit com a estrutura implementada

---
+++ B2.c
@@ -0,0 +1,201 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+/**
+ URL da CGI
+ */
+#define SCRIPT		"http://127.0.0.1/cgi-bin/cartas"
+/**
+ URL da pasta com as cartas
+ */
+#define BARALHO		"http://127.0.0.1/cards"
+
+/**
+ Ordem dos naipes
+ */
+#define NAIPES		"DCHS"
+/**
+ Ordem das cartas
+ */
+#define VALORES		"3456789TJQKA2"
+
+//#define ESTADO "%lld" //basicamente é uma string, só está aqui para diferenciar o estado
+
+typedef struct {
+    long long int mao[4];
+    long long int selected;
+} database;
+
+
+//database STR2ESTADO (char *query){
+//    database data = {{1},0};
+//    return data;
+//}
+/** \brief Devolve o índice da carta
+ 
+ @param naipe	O naipe da carta (inteiro entre 0 e 3)
+ @param valor	O valor da carta (inteiro entre 0 e 12)
+ @return		O índice correspondente à carta
+ */
+int indice(int naipe, int valor) {
+    return naipe * 13 + valor;
+}
+
+/** \brief Adiciona uma carta ao estado
+ 
+ @param ESTADO	O estado atual
+ @param naipe	O naipe da carta (inteiro entre 0 e 3)
+ @param valor	O valor da carta (inteiro entre 0 e 12)
+ @return		O novo estado
+ */
+long long int add_carta(long long int ESTADO, int naipe, int valor) {
+    int idx = indice(naipe, valor);
+    return ESTADO | ((long long int) 1 << idx);
+}
+
+/** \brief Remove uma carta do estado
+ 
+ @param ESTADO	O estado atual
+ @param naipe	O naipe da carta (inteiro entre 0 e 3)
+ @param valor	O valor da carta (inteiro entre 0 e 12)
+ @return		O novo estado
+ */
+long long int rem_carta(long long int ESTADO, int naipe, int valor) {
+    int idx = indice(naipe, valor);
+    return ESTADO & ~((long long int) 1 << idx);
+}
+
+/** \brief Verifica se uma carta pertence ao estado
+ 
+ @param ESTADO	O estado atual
+ @param naipe	O naipe da carta (inteiro entre 0 e 3)
+ @param valor	O valor da carta (inteiro entre 0 e 12)
+ @return		1 se a carta existe e 0 caso contrário
+ */
+int carta_existe(long long int ESTADO, int naipe, int valor) {
+    int idx = indice(naipe, valor);
+    return (ESTADO >> idx) & 1;
+}
+
+int carta_existe2 (long long int mao,int idx){
+    return (mao >> idx) & 1;
+}
+
+/** \brief Distribui as cartas pelas mãos de cada um dos jogadores
+*/
+database distribui(){
+    int maoCount[4]={0};
+    database data={{0x0000000000000},0x0000000000000};
+    int n,v,j;
+    for(n=0;n<4;n++){
+        v = 0;
+        while(v<13){
+            j = rand()%4;
+            if (maoCount[j]<13){
+                data.mao[j] = add_carta(data.mao[j],n,v);
+                maoCount[j]++;
+                v++;
+            }
+        }
+    }
+    return data;
+}
+
+/** \brief Imprime o html correspondente a uma carta
+ 
+ @param path	o URL correspondente à pasta que contém todas as cartas
+ @param x A coordenada x da carta
+ @param y A coordenada y da carta
+ @param ESTADO	O estado atual
+ @param naipe	O naipe da carta (inteiro entre 0 e 3)
+ @param valor	O valor da carta (inteiro entre 0 e 12)
+ */
+void imprime_carta(char *path, int x, int y, long long int ESTADO, int naipe, int valor) {
+    char *suit = NAIPES;
+    char *rank = VALORES;
+    char script[10240];
+    sprintf(script, "%s?q=%lld", SCRIPT, rem_carta(ESTADO, naipe, valor));
+    printf("<a xlink:href = \"%s\"><image x = \"%d\" y = \"%d\" height = \"110\" width = \"80\" xlink:href = \"%s/%c%c.svg\" /></a>\n", script, x, y, path, rank[valor], suit[naipe]);
+}
+
+/** \brief Imprime o estado
+ 
+ Esta função está a imprimir o estado em quatro colunas: uma para cada naipe
+ @param path	o URL correspondente à pasta que contém todas as cartas
+ @param data	O estado atual
+ */
+void imprime(char *path, database data) {
+    int n, v, p;
+    int x, y,ind;
+    
+    printf("<svg height = \"800\" width = \"800\">\n");
+    printf("<rect x = \"0\" y = \"0\" height = \"800\" width = \"800\" style = \"fill:#007700\"/>\n");
+    
+    for(y = 10, p = 0; p < 2; p++, y += 420) {
+        for(x = 100, ind = 0; ind < 52; ind++){
+            if(carta_existe2(data.mao[p],ind)){
+                n = ind/13;
+                v = ind%13;
+                imprime_carta(path,x,y,data.mao[p],n,v);
+                x+=20;
+            }
+        }
+    }
+    for(x = 10, p = 2; p < 4; p++, x += 460) {
+        for(y = 60, ind = 0; ind < 52; ind++){
+            if(carta_existe2(data.mao[p],ind)){
+                n = ind/13;
+                v = ind%13;
+                imprime_carta(path,x,y,data.mao[p], n,v);
+                y += 20;
+            }
+        }
+    }
+    printf("</svg>\n");
+}
+
+/** \brief Trata os argumentos da CGI
+ 
+ Esta função recebe a query que é passada à cgi-bin e trata-a.
+ Neste momento, a query contém o estado que é um inteiro que representa um conjunto de cartas.
+ Cada carta corresponde a um bit que está a 1 se essa carta está no conjunto e a 0 caso contrário.
+ Caso não seja passado nada à cgi-bin, ela assume que todas as cartas estão presentes.
+ @param query A query que é passada à cgi-bin
+ */
+void parse(){//char *query) {
+    database data;
+    //if(query!=NULL && strlen(query) != 0) { //onde tinha %qlld subsitui por %s; n sei para q é preciso a primeira condição...
+    //    data = STR2ESTADO(query);
+    //} else {
+        data = distribui();
+   // }
+    imprime(BARALHO, data);
+}
+
+
+/** \brief Função principal
+ 
+ Função principal do programa que imprime os cabeçalhos necessários e depois disso invoca
+ a função que vai imprimir o código html para desenhar as cartas
+ */
+int main() {
+    /*
+     * Cabeçalhos necessários numa CGI
+     */
+    printf("Content-Type: text/html; charset=utf-8\n\n");
+    printf("<header><title>Exemplo</title></header>\n");
+    printf("<body>\n");
+    
+    printf("<h1>Exemplo de utilização</h1>\n");
+    
+    /*
+     * Ler os valores passados à cgi que estão na variável ambiente e passá-los ao programa
+     */
+    srand(time(NULL));
+    //parse(getenv("QUERY_STRING"));
+    parse();
+    printf("</body>\n");
+    return 0;
+}
