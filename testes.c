38c0f3f1a4d4a31b9929edd1667bc33422c4a919
treishy <treishgame@gmail.com>
2016-03-26 18:35:58 +0000

    atuaprimaluis

--- testes.c
+++ testes.c
@@ -6,22 +6,22 @@
 /**
  URL da CGI
  */
-#define SCRIPT		"http://127.0.0.1/cgi-bin/cartas"
+#define SCRIPT      "http://127.0.0.1/cgi-bin/cartas"
 /**
  URL da pasta com as cartas
  */
-#define BARALHO		"http://127.0.0.1/cards"
+#define BARALHO     "http://127.0.0.1/cards"
 
 /**
  Ordem dos naipes
  */
-#define NAIPES		"DCHS"
+#define NAIPES      "DCHS"
 /**
  Ordem das cartas
  */
-#define VALORES		"3456789TJQKA2"
+#define VALORES     "3456789TJQKA2"
 
-#define DATA "%lld_%lld_%lld_%lld_%lld_%lld_%lld_%lld_%lld_%d_%d_%d_%d_%d_%d"
+#define DATA "%lld_%lld_%lld_%lld_%lld_%lld_%lld_%lld_%lld_%d_%d_%d_%d_%d_%d_%d"
 
 typedef long long int MAO;
 struct database{
@@ -31,6 +31,7 @@ struct database{
     int play;
     int nc; //número de cartas a serem jogadas por cada jogador
     int passar[4];
+    int inicio;
 };
 typedef struct database DATABASE;
 
@@ -40,9 +41,9 @@ typedef struct database DATABASE;
 //}
 /** \brief Devolve o índice da carta
  
- @param naipe	O naipe da carta (inteiro entre 0 e 3)
- @param valor	O valor da carta (inteiro entre 0 e 12)
- @return		O índice correspondente à carta
+ @param naipe   O naipe da carta (inteiro entre 0 e 3)
+ @param valor   O valor da carta (inteiro entre 0 e 12)
+ @return        O índice correspondente à carta
  */
 int indice(int naipe, int valor) {
     return naipe * 13 + valor;
@@ -50,10 +51,10 @@ int indice(int naipe, int valor) {
 
 /** \brief Adiciona uma carta a um dos estados
  
- @param ESTADO	O estado atual
- @param naipe	O naipe da carta (inteiro entre 0 e 3)
- @param valor	O valor da carta (inteiro entre 0 e 12)
- @return		O novo estado
+ @param ESTADO  O estado atual
+ @param naipe   O naipe da carta (inteiro entre 0 e 3)
+ @param valor   O valor da carta (inteiro entre 0 e 12)
+ @return        O novo estado
  */
 long long int add_carta(MAO ESTADO, int naipe, int valor) {
     int idx = indice(naipe, valor);
@@ -62,10 +63,10 @@ long long int add_carta(MAO ESTADO, int naipe, int valor) {
 
 /** \brief Remove uma carta a um dos estados
  
- @param ESTADO	O estado atual
- @param naipe	O naipe da carta (inteiro entre 0 e 3)
- @param valor	O valor da carta (inteiro entre 0 e 12)
- @return		O novo estado
+ @param ESTADO  O estado atual
+ @param naipe   O naipe da carta (inteiro entre 0 e 3)
+ @param valor   O valor da carta (inteiro entre 0 e 12)
+ @return        O novo estado
  */
 long long int rem_carta(MAO ESTADO, int naipe, int valor) {
     int idx = indice(naipe, valor);
@@ -74,10 +75,10 @@ long long int rem_carta(MAO ESTADO, int naipe, int valor) {
 
 /** \brief Verifica se uma carta pertence a um dos estados
  
- @param ESTADO	O estado atual
- @param naipe	O naipe da carta (inteiro entre 0 e 3)
- @param valor	O valor da carta (inteiro entre 0 e 12)
- @return		1 se a carta existe e 0 caso contrário
+ @param ESTADO  O estado atual
+ @param naipe   O naipe da carta (inteiro entre 0 e 3)
+ @param valor   O valor da carta (inteiro entre 0 e 12)
+ @return        1 se a carta existe e 0 caso contrário
  */
 int carta_existe(MAO ESTADO, int naipe, int valor) {
     int idx = indice(naipe, valor);
@@ -100,7 +101,7 @@ void DATA2STR(char * str,DATABASE data, int n, int v){
     else
         data.selected = rem_carta(data.selected,n,v);
     data.play = 0;
-    sprintf(str,DATA,data.mao[0],data.mao[1],data.mao[2],data.mao[3],data.selected,data.jogadas[0],data.jogadas[1],data.jogadas[2],data.jogadas[3],data.play,data.nc,data.passar[0],data.passar[1],data.passar[2],data.passar[3]);//TODO: a string com os lld's acho que dá para subsituir por uma palavra com um "define" no topo
+    sprintf(str,DATA,data.mao[0],data.mao[1],data.mao[2],data.mao[3],data.selected,data.jogadas[0],data.jogadas[1],data.jogadas[2],data.jogadas[3],data.play,data.nc,data.passar[0],data.passar[1],data.passar[2],data.passar[3],data.inicio);//TODO: a string com os lld's acho que dá para subsituir por uma palavra com um "define" no topo
 }
 
 //Dá a string que fica no link do botao play
@@ -119,13 +120,13 @@ void DATA2STR_botao(char * str, DATABASE data){
     for(i=0;i<4;i++){
         data.passar[i]=0;
     }
-    sprintf(str,DATA,data.mao[0],data.mao[1],data.mao[2],data.mao[3],data.selected,data.jogadas[0],data.jogadas[1],data.jogadas[2],data.jogadas[3],data.play,data.nc,data.passar[0],data.passar[1],data.passar[2],data.passar[3]);
+    sprintf(str,DATA,data.mao[0],data.mao[1],data.mao[2],data.mao[3],data.selected,data.jogadas[0],data.jogadas[1],data.jogadas[2],data.jogadas[3],data.play,data.nc,data.passar[0],data.passar[1],data.passar[2],data.passar[3],data.inicio);
 }
 
 //Passa a string que recebemos do browser para a nossa estrutura
 DATABASE STR2DATA(char * str){
     DATABASE data;
-    sscanf(str, DATA,&(data.mao[0]),&(data.mao[1]),&(data.mao[2]),&(data.mao[3]),&(data.selected),&(data.jogadas[0]),&(data.jogadas[1]),&(data.jogadas[2]),&(data.jogadas[3]),&data.play,&data.nc,&data.passar[0],&data.passar[1],&data.passar[2],&data.passar[3]);
+    sscanf(str, DATA,&(data.mao[0]),&(data.mao[1]),&(data.mao[2]),&(data.mao[3]),&(data.selected),&(data.jogadas[0]),&(data.jogadas[1]),&(data.jogadas[2]),&(data.jogadas[3]),&data.play,&data.nc,&data.passar[0],&data.passar[1],&data.passar[2],&data.passar[3],&data.inicio);
     return data;
 }
 
@@ -152,12 +153,12 @@ DATABASE distribui(DATABASE data){
 
 /** \brief Imprime o html correspondente a uma carta
  
- @param path	o URL correspondente à pasta que contém todas as cartas
+ @param path    o URL correspondente à pasta que contém todas as cartas
  @param x A coordenada x da carta
  @param y A coordenada y da carta
- @param ESTADO	O estado atual
- @param naipe	O naipe da carta (inteiro entre 0 e 3)
- @param valor	O valor da carta (inteiro entre 0 e 12)
+ @param ESTADO  O estado atual
+ @param naipe   O naipe da carta (inteiro entre 0 e 3)
+ @param valor   O valor da carta (inteiro entre 0 e 12)
  */
 void imprime_carta(char *path, int x, int y, DATABASE data, int naipe, int valor) {
     char *suit = NAIPES;
@@ -184,7 +185,7 @@ void imprime_play (char * path, DATABASE data){//TODO:meter para if combinação
     int i;
     data.jogadas[0]=0;
     DATA2STR_botao(script,data);
-    printf("<a xlink:href = \"cartas?%s\"><image x = \"600\" y = \"450\" height = \"60\" width = \"30\" xlink:href = \"%s/botao.svg\" /></a>\n", script, path);
+    printf("<a xlink:href = \"cartas?%s\"><image x = \"560\" y = \"460\" height = \"30\" width = \"90\" xlink:href = \"%s/botao_play.svg\" /></a>\n", script, path);
     //else
     // imprimir só imagem do botão desvanecido
 }
@@ -197,8 +198,8 @@ void imprime_passar (char * path, DATABASE data){
     data.jogadas[0]=0;
     data.play = 1;
     data.passar[0]=1;
-    sprintf(script,DATA,data.mao[0],data.mao[1],data.mao[2],data.mao[3],data.selected,data.jogadas[0],data.jogadas[1],data.jogadas[2],data.jogadas[3],data.play,data.nc,data.passar[0],data.passar[1],data.passar[2],data.passar[3]);
-    printf("<a xlink:href = \"cartas?%s\"><image x = \"650\" y = \"450\" height = \"60\" width = \"30\" xlink:href = \"%s/botao.svg\" /></a>\n", script, path);
+    sprintf(script,DATA,data.mao[0],data.mao[1],data.mao[2],data.mao[3],data.selected,data.jogadas[0],data.jogadas[1],data.jogadas[2],data.jogadas[3],data.play,data.nc,data.passar[0],data.passar[1],data.passar[2],data.passar[3],data.inicio);
+    printf("<a xlink:href = \"cartas?%s\"><image x = \"560\" y = \"500\" height = \"30\" width = \"90\" xlink:href = \"%s/botao_pass.svg\" /></a>\n", script, path);
 }
 
 
@@ -243,7 +244,7 @@ DATABASE comeca_bot(DATABASE data,int m){
 }
 
 DATABASE joga_bots(DATABASE data,int m){
-    int max,n,i,v,passou=0;
+    int max,n,i,v,passou;
     for(i=0;i<4;i++){
         n=maior_jogadas(data.jogadas[i])/13;
         v=maior_jogadas(data.jogadas[i])%13;
@@ -254,6 +255,7 @@ DATABASE joga_bots(DATABASE data,int m){
     //TODO: tem que testar, se os outros data.jogadas forem 0, pode jogar o que quiser, senão, faz o que está aqui em baixo, tem que se acrescentar à estrutura número da cartas a ser usado nesta jogada
     //retirar o break para dar com mais do que uma carta por jogador;
     if(all_passed(data,m)==0){
+        passou=0;
         for(i=0;i<52;i++){
             n=i/13;
             v=i%13;
@@ -276,11 +278,21 @@ DATABASE joga_bots(DATABASE data,int m){
     return data;
 }
 
+int quem_comeca(DATABASE data){
+    int i,r=0;
+    for(i=1;i<4;i++)
+        if(carta_existe(data.mao[i],0,0)==1){
+            r=i;
+            break;
+        }
+    return r;
+}
+
 /** \brief Imprime o estado
  
  Esta função está a imprimir o estado em quatro colunas: uma para cada naipe
- @param path	o URL correspondente à pasta que contém todas as cartas
- @param data	O estado atual
+ @param path    o URL correspondente à pasta que contém todas as cartas
+ @param data    O estado atual
  */
 void imprime(char *path, DATABASE data) {
     int n, v, p;
@@ -289,7 +301,14 @@ void imprime(char *path, DATABASE data) {
     
     printf("<svg height = \"800\" width = \"800\">\n");
     printf("<rect x = \"0\" y = \"0\" height = \"800\" width = \"800\" style = \"fill:#007700\"/>\n");
-    if(data.play == 1)
+    if(data.inicio == 1){
+        i = quem_comeca(data);
+        if (i!=0)
+            for(i;i<4;i++)
+                data = joga_bots(data,i);
+        data.inicio=2;
+    }
+    if(data.play == 1 && data.inicio==2)
         for(i=1;i<4;i++)
             data = joga_bots(data,i);
     for(y = 430, p = 0; p < 3; p+=2, y -= 420) {
@@ -316,8 +335,8 @@ void imprime(char *path, DATABASE data) {
                 
         }
     }
-    //TODO:fazer imprimir as cartas à frente de cada jgador e se n jogar nenhuma imprimir imagem "Pass" ->que se tem que fazer
-    for(x = 10, p = 1; p < 4; p+=2, x += 650) {
+    
+    for(x = 10, p = 3; p > 0; p-=2, x += 650) {
         for(y = 100, ind = 0; ind < 52; ind++){
             if(carta_existe2(data.mao[p],ind)==1){
                 n = ind/13;
@@ -329,33 +348,79 @@ void imprime(char *path, DATABASE data) {
     }
     for(x=300,y=150,i=2;i>=0;i-=2,y+=150){
         for(ind=0;ind<52;ind++)
-            if(carta_existe2(data.jogadas[i],ind)){ 
-                    n = ind/13;
-                    v = ind%13;
-                    imprime_carta_bot(path,x,y,n,v);
-                    x +=20;
+            if(carta_existe2(data.jogadas[i],ind)){
+                n = ind/13;
+                v = ind%13;
+                imprime_carta_bot(path,x,y,n,v);
+                x +=20;
             }
-    x=300;
+        x=300;
     }
-
-
-    for(x=150,y=200,i=1;i<4;i+=2,x+=350){
+    
+    
+    for(x=150,y=200,i=3;i>0;i-=2,x+=350){
         for(ind=0;ind<52;ind++)
-            if(carta_existe2(data.jogadas[i],ind)){ 
-                    n = ind/13;
-                    v = ind%13;
-                    imprime_carta_bot(path,x,y,n,v);
-                    y +=20;
+            if(carta_existe2(data.jogadas[i],ind)){
+                n = ind/13;
+                v = ind%13;
+                imprime_carta_bot(path,x,y,n,v);
+                y +=20;
             }
-    y=200;
+        y=200;
     }
-
+    
     //data.jogadas = 0; //remover de data.jogadas a carta quando imprimida antes?
     imprime_play(path,data);
     imprime_passar(path,data);
     printf("</svg>\n");    
 }
 
+void check_finish(char * path, DATABASE data){
+    int f=0,b;
+    for(b=0;b<4;b++)
+        if(data.mao[b]==0)
+            f=1;
+    if(f==1)
+        printf("Fim\n");
+    else
+        imprime(path,data);
+}
+
+void imprime_start(DATABASE data,char * path){
+    char script[52000];
+    sprintf(script,DATA,data.mao[0],data.mao[1],data.mao[2],data.mao[3],data.selected,data.jogadas[0],data.jogadas[1],data.jogadas[2],data.jogadas[3],data.play,data.nc,data.passar[0],data.passar[1],data.passar[2],data.passar[3],data.inicio);
+    printf("<a xlink:href = \"cartas?%s\"><image x = \"350\" y = \"250\" height = \"30\" width = \"90\" xlink:href = \"%s/botao_start.svg\" /></a>\n", script, path);
+}
+
+void start(char * path, DATABASE data){
+    int y, x,p,ind,n,v;
+    printf("<svg height = \"800\" width = \"800\">\n");
+    printf("<rect x = \"0\" y = \"0\" height = \"800\" width = \"800\" style = \"fill:#007700\"/>\n");
+    if(data.inicio==0){
+        for(y = 430, p = 0; p < 3; p+=2, y -= 420)
+            for(x = 200, ind = 0; ind < 52; ind++)
+                if(carta_existe2(data.mao[p],ind)==1){
+                    n = ind/13;
+                    v = ind%13;
+                    imprime_carta_bot(path,x,y,n,v);
+                    x += 20;
+                }
+        for(x = 10, p = 3; p > 0; p-=2, x += 650)
+            for(y = 100, ind = 0; ind < 52; ind++)
+                if(carta_existe2(data.mao[p],ind)==1){
+                    n = ind/13;
+                    v = ind%13;
+                    imprime_carta_bot(path,x,y,n,v);
+                    y += 20;
+                }
+        data.inicio=1;
+        imprime_start(data,path);
+        printf("</svg>\n");
+    }
+    else
+        imprime(path,data);
+    
+}
 /** \brief Trata os argumentos da CGI
  
  Esta função recebe a query que é passada à cgi-bin e trata-a.
@@ -365,12 +430,15 @@ void imprime(char *path, DATABASE data) {
  @param query A query que é passada à cgi-bin
  */
 void parse (char * query) {
-    DATABASE data = {{0},0,{0},0,0,{0}};
-    if(query!=NULL && strlen(query) != 0) //n sei para q é preciso a primeira condição...
+    DATABASE data = {{0},0,{0},0,0,{0},0};
+    if(query!=NULL && strlen(query) != 0){ //n sei para q é preciso a primeira condição...
         data = STR2DATA(query);
-    else
-        data = distribui(data);
-    imprime(BARALHO, data);
+        check_finish(BARALHO,data);
+    }
+    else{
+        data = distribui(data); //é a primeira jogada, quem será que tem o precioso 3 de ouros?
+        start(BARALHO, data);
+    }
 }
 
 
